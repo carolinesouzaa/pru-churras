0930f498607d74358dc7e33181893b9295a136a0
carolinesouzaa <carolinevmsouza@gmail.com>
2023-03-16 11:58:45 -0300

    Funcionamento do marcador de truco.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,8 +8,80 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  valor = 1;
+  ponto = 0;
+  ponto2 = 0;
+  partida = 0;
+  partida2 = 0;
 
-  constructor() {}
+  constructor(
+    private toastController: ToastController,
+    private alertController: AlertController
+  ) {}
+
+  add1() {
+    this.valor = 1;
+  }
+
+  add3() {
+    this.valor = 3;
+  }
+
+  add6() {
+    this.valor = 6;
+  }
+
+  add9() {
+    this.valor = 9;
+  }
+
+  add12() {
+    this.valor = 12;
+  }
+
+  async addValor() {
+    this.ponto += this.valor;
+    if (this.ponto >= 12) {
+      this.partida += 1;
+      this.ponto = 0;
+      this.ponto2 = 0;
+    }
+
+    this.valor = 1;
+  }
+
+  async addValor2() {
+    this.ponto2 += this.valor;
+    if (this.ponto2 >= 12) {
+      this.partida2 += 1;
+      this.ponto = 0;
+      this.ponto2 = 0;
+    }
+
+    this.valor = 1;
+  }
+
+  tiraValor() {
+    this.ponto -= this.valor;
+    this.ponto = this.ponto < 0 ? 0 : this.ponto;
+
+    this.valor = 1;
+  }
+
+  tiraValor2() {
+    this.ponto2 -= this.valor;
+    this.ponto2 = this.ponto < 0 ? 0 : this.ponto;
+
+    this.valor = 1;
+  }
+
+  limpar() {
+    this.valor = 1;
+    this.ponto = 0;
+    this.ponto2 = 0;
+    this.partida = 0;
+    this.partida2 = 0;
+  }
 
 }
 
